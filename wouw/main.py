788808f31687c87788ffffe1e4d232c56dada553
superlou <lousimons@gmail.com>
2020-12-28 10:44:33 -0500

    Trying to fix saving in Word

--- wouw/main.py
+++ wouw/main.py
@@ -5,6 +5,7 @@ import argparse
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from rich.console import Console
+from queue import Queue
 from .docx_reader import DocxReader
 
 
@@ -15,19 +16,20 @@ print = console.print
 def analyze_requirements_docx(filename):
     dr = DocxReader(filename, 'r')
     print('')
-    print('✓ Updated [color(2)]{}[/color(2)]'.format(dr.filename))
-    print('✓ {} requirements parsed'.format(len(dr.requirements)))
-    print('  └ Next requirement:', dr.next_requirement_id())
+    print('Updated [color(2)]{}[/color(2)]'.format(dr.filename))
+    print('{} requirements parsed'.format(len(dr.requirements)))
+    print('Next requirement:', dr.next_requirement_id())
 
 
 class EventHandler(FileSystemEventHandler):
-    def __init__(self, watched_file):
+    def __init__(self, watched_file, events):
         super().__init__()
         self.watched_file = watched_file
+        self.events = events
 
     def on_modified(self, event):
         if event.src_path == self.watched_file:
-            analyze_requirements_docx(event.src_path)
+            self.events.put(event.src_path)
 
 
 def main():
@@ -35,8 +37,10 @@ def main():
     parser.add_argument('file')
     args = parser.parse_args()
 
+    events = Queue()
+
     watched_file = args.file
-    event_handler = EventHandler(watched_file)
+    event_handler = EventHandler(watched_file, events)
     observer = Observer()
     observer.schedule(event_handler,
                       os.path.dirname(watched_file),
@@ -47,7 +51,13 @@ def main():
 
     try:
         while True:
-            time.sleep(1)
+            event = events.get()
+            try:
+                analyze_requirements_docx(event)
+            except IOError as e:
+                # MS Word locks files during its first save
+                pass
+
     except KeyboardInterrupt:
         observer.stop()
 
